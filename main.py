44cfbe62f992bb0b61dbbbb3b08ec1b25d6c4b3b
N.Sibiselva <sibiselvanallusamy123@gmail.com>
2022-11-21 17:23:53 +0530

    i

--- main.py
+++ main.py
@@ -25,6 +25,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-if __name__ == '__main__':
-    # defining server ip address and port
-    app.run()
+app.run()
